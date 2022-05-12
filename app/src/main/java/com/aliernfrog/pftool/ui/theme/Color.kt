1df2ccd12ea2ce120b9dea2dd3758a4f2fcf45c1
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-12 21:00:19 +0300

    Add MainActivity

---
+++ app/src/main/java/com/aliernfrog/pftool/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.aliernfrog.pftool.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundLight = Color(0xFFF2F2F2)
+val BackgroundDark = Color(0xFF000000)
+val LinearLight = Color(0xFFFFFFFF)
+val LinearDark = Color(0xFF202020)
+val ButtonLight = Color(0xFFE2E2E2)
+val ButtonDark = Color(0xFF0E0E0E)
