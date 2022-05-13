cccf5ef0eea716c84682f8d3583b5aa775174b60
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-13 22:17:05 +0300

    Fix window background

--- app/src/main/java/com/aliernfrog/pftool/MainActivity.kt
+++ app/src/main/java/com/aliernfrog/pftool/MainActivity.kt
@@ -3,12 +3,7 @@ package com.aliernfrog.pftool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -20,9 +15,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PFToolTheme {
-                Box(Modifier.fillMaxSize().background(MaterialTheme.colors.background)) {
-                    Navigation()
-                }
+                Navigation()
             }
         }
     }
