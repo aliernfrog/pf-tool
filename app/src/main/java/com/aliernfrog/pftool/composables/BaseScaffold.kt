54b0ce43be9ab1d94a93d55c755188b8a275b63b
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-13 21:30:23 +0300

    Add composables.BaseScaffold

---
+++ app/src/main/java/com/aliernfrog/pftool/composables/BaseScaffold.kt
@@ -0,0 +1,31 @@
+package com.aliernfrog.pftool.composables
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun BaseScaffold(title: String, content: @Composable() (ColumnScope.() -> Unit)) {
+    Scaffold(
+        scaffoldState = rememberScaffoldState(),
+        topBar = {
+            TopAppBar(backgroundColor = MaterialTheme.colors.secondary,
+                contentPadding = PaddingValues(horizontal = 24.dp),
+                content = { Text(text = title, color = MaterialTheme.colors.onSecondary) })
+        },
+        content = {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+                    .verticalScroll(rememberScrollState()),
+                content = content)
+        }
+    )
+}
--- app/src/main/java/com/aliernfrog/pftool/screens/MainScreen.kt
+++ app/src/main/java/com/aliernfrog/pftool/screens/MainScreen.kt
@@ -1,30 +1,16 @@
 package com.aliernfrog.pftool.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import com.aliernfrog.pftool.R
+import com.aliernfrog.pftool.composables.BaseScaffold
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
-    Scaffold(
-        scaffoldState = rememberScaffoldState(),
-        topBar = {
-            TopAppBar(backgroundColor = MaterialTheme.colors.secondary,
-                contentPadding = PaddingValues(horizontal = 24.dp),
-                content = { Text(text = LocalContext.current.getString(R.string.app_name), color = MaterialTheme.colors.onSecondary) })
-        },
-        content = {
-            Column(Modifier.fillMaxWidth().padding(horizontal = 24.dp).verticalScroll(rememberScrollState())) {
-                Text("test\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\n")
-            }
-        }
-    )
+    BaseScaffold(title = LocalContext.current.getString(R.string.app_name), content = {
+        Text("test\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\n")
+    })
 }
