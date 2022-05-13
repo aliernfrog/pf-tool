f2be128286b1bac6594612f587a663655d034996
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-13 23:31:19 +0300

    Add maps and options buttons (no function)

---
+++ app/src/main/java/com/aliernfrog/pftool/composables/MainButton.kt
@@ -0,0 +1,36 @@
+package com.aliernfrog.pftool.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MainButton(title: String, description: String, painter: Painter, backgroundColor: Color, contentColor: Color, onClick: () -> Unit) {
+    Button(modifier = Modifier
+        .padding(all = 8.dp)
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(20),
+        colors = ButtonDefaults.buttonColors(backgroundColor = backgroundColor, contentColor = contentColor),
+        onClick = onClick,
+        contentPadding = PaddingValues(all = 8.dp),
+        content = {
+            Image(painter, title)
+            Column(Modifier.fillMaxWidth().padding(start = 8.dp)) {
+                Text(title)
+                Text(description, Modifier.alpha(0.8f), fontSize = 12.sp)
+            }
+        })
+}
--- app/src/main/java/com/aliernfrog/pftool/screens/MainScreen.kt
+++ app/src/main/java/com/aliernfrog/pftool/screens/MainScreen.kt
@@ -1,16 +1,36 @@
 package com.aliernfrog.pftool.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import com.aliernfrog.pftool.R
 import com.aliernfrog.pftool.composables.BaseScaffold
+import com.aliernfrog.pftool.composables.MainButton
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
     BaseScaffold(title = LocalContext.current.getString(R.string.app_name), content = {
-        Text("test\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\n=== end ===")
+        val context = LocalContext.current
+        MainButton(
+            title = context.getString(R.string.manageMaps),
+            description = context.getString(R.string.manageMapsDescription),
+            painter = painterResource(id = R.drawable.map),
+            backgroundColor = MaterialTheme.colors.secondary,
+            contentColor = MaterialTheme.colors.onSecondary,
+            onClick = {
+                //TODO
+            })
+        MainButton(
+            title = context.getString(R.string.options),
+            description = context.getString(R.string.optionsDescription),
+            painter = painterResource(id = R.drawable.options),
+            backgroundColor = MaterialTheme.colors.secondary,
+            contentColor = MaterialTheme.colors.onSecondary,
+            onClick = {
+                //TODO
+            })
     })
 }
