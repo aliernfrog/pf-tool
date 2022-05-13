4d40fb41d47fa3a4c4c6c5a4aa0ce6de9bb1759b
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-13 20:32:24 +0300

    Update color palettes

--- app/src/main/java/com/aliernfrog/pftool/ui/theme/Theme.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/theme/Theme.kt
@@ -5,26 +5,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
     background = BackgroundLight,
-    primary = LinearLight,
-    primaryVariant = ButtonLight
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    surface = BackgroundLight,
+    primary = Blue,
+    primaryVariant = Blue,
+    secondary = LinearLight,
+    secondaryVariant = ButtonLight,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onSurface = Color.Black
 )
 
 private val DarkColorPalette = darkColors(
     background = BackgroundDark,
-    primary = LinearDark,
-    primaryVariant = ButtonDark
+    surface = BackgroundDark,
+    primary = Blue,
+    primaryVariant = Blue,
+    secondary = LinearDark,
+    secondaryVariant = ButtonDark,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White
 )
 
 @Composable
