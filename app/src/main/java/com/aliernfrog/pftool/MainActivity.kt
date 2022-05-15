220369c55b3d00eaf3436f12e55a831d7286c394
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-15 09:30:18 +0300

    Add box to prevent splash when navigating

--- app/src/main/java/com/aliernfrog/pftool/MainActivity.kt
+++ app/src/main/java/com/aliernfrog/pftool/MainActivity.kt
@@ -3,8 +3,12 @@ package com.aliernfrog.pftool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -18,6 +22,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PFToolTheme {
+                Box(modifier = Modifier.background(MaterialTheme.colors.background).fillMaxSize())
                 SystemBars()
                 Navigation()
             }
