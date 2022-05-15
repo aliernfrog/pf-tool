40b120094df17ed02d3e8303b3b71c1ab71b49ca
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-15 15:34:59 +0300

    Revert "Update OptionsScreen text for testing"
    This reverts commit 7c6a917d0cd64f02825fd71283c292253c4d48c5.

--- app/src/main/java/com/aliernfrog/pftool/screens/OptionsScreen.kt
+++ app/src/main/java/com/aliernfrog/pftool/screens/OptionsScreen.kt
@@ -10,6 +10,6 @@ import com.aliernfrog.pftool.composables.BaseScaffold
 @Composable
 fun OptionsScreen(navController: NavController) {
     BaseScaffold(title = LocalContext.current.getString(R.string.options), navController = navController) {
-        Text(text = "Options will be available here\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\n=== END ===")
+        Text(text = "Options will be available here")
     }
 }
