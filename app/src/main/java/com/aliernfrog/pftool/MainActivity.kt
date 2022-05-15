96a1492638fe4b6c324d9dfa94d11a8c38a867db
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-15 09:18:54 +0300

    System bars color fit theme

--- app/src/main/java/com/aliernfrog/pftool/MainActivity.kt
+++ app/src/main/java/com/aliernfrog/pftool/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aliernfrog.pftool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -10,15 +11,23 @@ import androidx.navigation.compose.rememberNavController
 import com.aliernfrog.pftool.screens.MainScreen
 import com.aliernfrog.pftool.screens.OptionsScreen
 import com.aliernfrog.pftool.ui.theme.PFToolTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PFToolTheme {
+                SystemBars()
                 Navigation()
             }
         }
+    }
+
+    @Composable
+    fun SystemBars() {
+        val systemUiController = rememberSystemUiController()
+        systemUiController.setSystemBarsColor(MaterialTheme.colors.background)
     }
 
     @Composable
