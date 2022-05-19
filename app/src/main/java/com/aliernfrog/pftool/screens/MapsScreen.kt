06e1b0fba50b825a1909ced70f1fba91927607df
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-19 12:02:47 +0300

    Load basic map info

--- app/src/main/java/com/aliernfrog/pftool/screens/MapsScreen.kt
+++ app/src/main/java/com/aliernfrog/pftool/screens/MapsScreen.kt
@@ -1,24 +1,35 @@
 package com.aliernfrog.pftool.screens
 
+import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.aliernfrog.pftool.R
 import com.aliernfrog.pftool.composables.BaseScaffold
+import com.aliernfrog.pftool.composables.ColumnRounded
 import com.aliernfrog.pftool.composables.MainButton
 import com.aliernfrog.pftool.utils.UriToFileUtil
+import java.io.File
+
+private var mapPath = mutableStateOf("")
+private var mapNameEdit = mutableStateOf("")
 
 @Composable
 fun MapsScreen(navController: NavController) {
     val context = LocalContext.current
     BaseScaffold(title = context.getString(R.string.manageMaps), navController = navController) {
         PickMapFileButtton()
+        MapName()
     }
 }
 
@@ -28,7 +39,7 @@ fun PickMapFileButtton() {
     val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         val convertedPath = UriToFileUtil.getRealFilePath(it.data?.data!!, context)
         if (convertedPath != null) {
-            Toast.makeText(context, convertedPath, Toast.LENGTH_SHORT).show()
+            getMap(convertedPath, context)
         } else {
             Toast.makeText(context, context.getString(R.string.warning_couldntConvertToPath), Toast.LENGTH_SHORT).show()
         }
@@ -41,5 +52,32 @@ fun PickMapFileButtton() {
     ) {
         val intent = Intent(Intent.ACTION_GET_CONTENT).setType("application/zip").putExtra(Intent.EXTRA_LOCAL_ONLY, true)
         launcher.launch(intent)
+    }
+}
+
+@Composable
+fun MapName() {
+    if (mapPath.value != "") {
+        val context = LocalContext.current
+        val file = File(mapPath.value)
+        ColumnRounded(title = context.getString(R.string.manageMapsMapName)) {
+            OutlinedTextField(
+                value = mapNameEdit.value,
+                placeholder = { Text(file.name) },
+                onValueChange = { mapNameEdit.value = it},
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+fun getMap(path: String, context: Context) {
+    val file = File(path)
+    if (file.exists()) {
+        mapPath.value = file.absolutePath
+        mapNameEdit.value = file.name
+    } else {
+        Toast.makeText(context, context.getString(R.string.warning_fileDoesntExist), Toast.LENGTH_SHORT).show()
     }
 }
