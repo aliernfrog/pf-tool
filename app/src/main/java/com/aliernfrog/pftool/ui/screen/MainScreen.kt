42a77b57a69cea71cef181b1e122de5d58c6f9a1
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-19 20:22:05 +0300

    Only show permission sheet if no permissions + disable maps button if no permissions

--- app/src/main/java/com/aliernfrog/pftool/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/screen/MainScreen.kt
@@ -1,22 +1,32 @@
 package com.aliernfrog.pftool.ui.screen
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Environment
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import com.aliernfrog.pftool.R
 import com.aliernfrog.pftool.ui.composable.PFToolBaseScaffold
 import com.aliernfrog.pftool.ui.composable.PFToolButton
 import com.aliernfrog.pftool.ui.sheets.PermissionSheet
 
+val hasPermissions = mutableStateOf(true)
+
 @Composable
 fun MainScreen(navController: NavController) {
+    val context = LocalContext.current
     PFToolBaseScaffold(title = LocalContext.current.getString(R.string.app_name), navController = navController) {
-        val context = LocalContext.current
         PFToolButton(
             title = context.getString(R.string.manageMaps),
             description = context.getString(R.string.manageMapsDescription),
             painter = painterResource(id = R.drawable.map),
+            enabled = hasPermissions.value,
             onClick = {
                 navController.navigate("maps")
             }
@@ -30,5 +40,11 @@ fun MainScreen(navController: NavController) {
             }
         )
     }
-    PermissionSheet()
+    if (!checkPermissions(context)) PermissionSheet()
+}
+
+private fun checkPermissions(context: Context): Boolean {
+    hasPermissions.value = if (Build.VERSION.SDK_INT >= 30) Environment.isExternalStorageManager()
+    else ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+    return hasPermissions.value
 }
