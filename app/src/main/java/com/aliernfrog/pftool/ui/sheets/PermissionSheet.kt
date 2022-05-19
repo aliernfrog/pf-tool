c8599f67c42ba6d86273517fe41c089ace6546ac
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-05-19 18:35:49 +0300

    Hide sheet on OK click + fix color

--- app/src/main/java/com/aliernfrog/pftool/ui/sheets/PermissionSheet.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/sheets/PermissionSheet.kt
@@ -2,24 +2,27 @@ package com.aliernfrog.pftool.ui.sheets
 
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import com.aliernfrog.pftool.R
 import com.aliernfrog.pftool.ui.composable.PFToolButton
 import com.aliernfrog.pftool.ui.composable.PFToolColumnRounded
 import com.aliernfrog.pftool.ui.composable.PFToolRoundedModalBottomSheet
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PermissionSheet() {
     val context = LocalContext.current
     val state = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Expanded)
+    val scope = rememberCoroutineScope()
     PFToolRoundedModalBottomSheet(title = context.getString(R.string.warning_missingPermissions), state) {
         PFToolColumnRounded {
             Text(context.getString(R.string.info_storagePermission), fontWeight = FontWeight.Bold)
         }
-        PFToolButton(title = context.getString(R.string.action_ok), backgroundColor = MaterialTheme.colors.primary) {
-
+        PFToolButton(title = context.getString(R.string.action_ok), contentColor = MaterialTheme.colors.onPrimary, backgroundColor = MaterialTheme.colors.primary) {
+            scope.launch { state.hide() }
         }
     }
 }
