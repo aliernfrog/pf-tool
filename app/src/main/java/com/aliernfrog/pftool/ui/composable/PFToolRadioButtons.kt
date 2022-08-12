84e1f84d53c6a136ece94de7579cdd3e1cbd9cf7
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-08-12 22:17:40 +0300

    feat: update PFToolRadioButtons

--- app/src/main/java/com/aliernfrog/pftool/ui/composable/PFToolRadioButtons.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/composable/PFToolRadioButtons.kt
@@ -1,8 +1,9 @@
 package com.aliernfrog.pftool.ui.composable
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.RadioButtonDefaults
@@ -12,8 +13,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PFToolRadioButtons(options: List<String>, columnColor: Color = MaterialTheme.colors.secondary, selectedIndex: Int = 0, onSelect: (String) -> Unit) {
@@ -28,7 +31,8 @@ fun PFToolRadioButtons(options: List<String>, columnColor: Color = MaterialTheme
             Row(verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .selectable(selected = isSelected, onClick = { onSelected() })) {
+                    .clip(RoundedCornerShape(20.dp))
+                    .clickable { onSelected() }) {
                 RadioButton(colors = RadioButtonDefaults.colors(selectedColor = MaterialTheme.colors.primary),
                     selected = isSelected,
                     onClick = { onSelected() }
