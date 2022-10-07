bb4f638f927e54fbe908172b54b9050b84e0fb69
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-10-07 22:39:42 +0300

    feat: navbar

--- app/src/main/java/com/aliernfrog/pftool/ui/composable/PFToolBaseScaffold.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/composable/PFToolBaseScaffold.kt
@@ -1,6 +1,7 @@
 package com.aliernfrog.pftool.ui.composable
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
@@ -13,12 +14,17 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.aliernfrog.pftool.NavRoutes
 import com.aliernfrog.pftool.R
+import com.aliernfrog.pftool.data.Screen
 import com.aliernfrog.pftool.util.GeneralUtil
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,10 +51,32 @@ fun PFToolBaseScaffold(title: String, navController: NavController, onNavigation
                 scrollBehavior = scrollBehavior
             )
         },
+        bottomBar = { BottomBar(navController) }
     ) { padding ->
         Column(Modifier.fillMaxWidth().verticalScroll(rememberScrollState()).animateContentSize().padding(padding)) {
             content()
             Spacer(Modifier.height(60.dp+GeneralUtil.getNavigationBarHeight()))
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(navController: NavController) {
+    val context = LocalContext.current
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = backStackEntry.value?.destination?.route
+    val navScreens = listOf(
+        Screen(NavRoutes.MAPS, context.getString(R.string.manageMaps), painterResource(id = R.drawable.map)),
+        Screen(NavRoutes.OPTIONS, context.getString(R.string.options), painterResource(id = R.drawable.options))
+    )
+    BottomAppBar {
+        navScreens.forEach {
+            NavigationBarItem(
+                selected = it.route == currentRoute,
+                onClick = { navController.navigate(it.route) { popUpTo(0) } },
+                icon = { Image(it.icon, it.name, colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface), modifier = Modifier.size(28.dp)) },
+                label = { Text(it.name, modifier = Modifier.offset(y = 5.dp)) }
+            )
         }
     }
 }
