7bc55d2731222afa11f66e578c7691d2483cf9d1
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-10-21 22:15:09 +0300

    feat: better thumbnails

--- app/src/main/java/com/aliernfrog/pftool/ui/composable/PFToolMapButton.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/composable/PFToolMapButton.kt
@@ -1,15 +1,21 @@
 package com.aliernfrog.pftool.ui.composable
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -30,26 +36,35 @@ fun PFToolMapButton(
 ) {
     val context = LocalContext.current
     var thumbnailLoaded by remember { mutableStateOf(false) }
-    Button(
-        onClick = onClick,
-        modifier = Modifier.padding(8.dp).fillMaxWidth(),
-        shape = PFToolComposableShape,
-        colors = ButtonDefaults.buttonColors(containerColor = containerColor, contentColor = contentColor),
-        contentPadding = PaddingValues(8.dp)
-    ) {
+    Box(Modifier.fillMaxWidth().height(IntrinsicSize.Max).padding(8.dp).clip(PFToolComposableShape).background(containerColor).clickable { onClick() }) {
         AsyncImage(
             model = map.thumbnailPainterModel,
             contentDescription = null,
-            modifier = Modifier.padding(end = 4.dp).size(40.dp).padding(4.dp),
-            placeholder = painterResource(R.drawable.map),
-            error = painterResource(R.drawable.map),
-            fallback = painterResource(R.drawable.map),
-            onSuccess = { thumbnailLoaded = true },
-            colorFilter = if (!thumbnailLoaded) ColorFilter.tint(contentColor) else null
+            modifier = Modifier.fillMaxWidth().heightIn(if (thumbnailLoaded) 70.dp else 0.dp),
+            placeholder = ColorPainter(containerColor),
+            error = ColorPainter(containerColor),
+            fallback = ColorPainter(containerColor),
+            onSuccess = { thumbnailLoaded = false },
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
         )
-        Column(Modifier.fillMaxWidth()) {
-            Text(map.name, fontWeight = FontWeight.Bold, fontSize = 16.sp)
-            Text(FileUtil.lastModifiedFromLong(map.lastModified, context), Modifier.alpha(0.8f), fontSize = 12.sp)
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .background(Brush.horizontalGradient(listOf(containerColor,Color.Transparent), startX = 400f))
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.map),
+                contentDescription = null,
+                modifier = Modifier.padding(end = 4.dp).size(40.dp).padding(4.dp),
+                colorFilter = ColorFilter.tint(contentColor)
+            )
+            Column {
+                Text(text = map.name, color = contentColor, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                Text(text = FileUtil.lastModifiedFromLong(map.lastModified, context), modifier = Modifier.alpha(0.9f), color = contentColor, fontSize = 12.sp)
+            }
         }
     }
 }
