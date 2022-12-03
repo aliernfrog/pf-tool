5d10e682052e5172504ceeacc66a4789f5b90ec4
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-12-03 18:43:48 +0300

    feat(MapButton): adjust ripple color

--- app/src/main/java/com/aliernfrog/pftool/ui/composable/PFToolMapButton.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/composable/PFToolMapButton.kt
@@ -2,13 +2,16 @@ package com.aliernfrog.pftool.ui.composable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.PinDrop
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -36,7 +39,11 @@ fun PFToolMapButton(
     onClick: () -> Unit
 ) {
     val context = LocalContext.current
-    Box(Modifier.fillMaxWidth().height(IntrinsicSize.Max).padding(8.dp).clip(PFToolComposableShape).background(containerColor).clickable { onClick() }) {
+    Box(Modifier.fillMaxWidth().height(IntrinsicSize.Max).padding(8.dp).clip(PFToolComposableShape).background(containerColor).clickable(
+        interactionSource = remember { MutableInteractionSource() },
+        indication = rememberRipple(color = contentColor),
+        onClick = onClick
+    )) {
         if (showMapThumbnail) AsyncImage(
             model = map.thumbnailPainterModel,
             contentDescription = null,
