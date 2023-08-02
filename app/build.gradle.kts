9a28ee6694219352a428659e8c53db249a0d355c
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-08-02 08:15:30 +0300

    chore: update target SDK, Gradle, AGP, Kotlin and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,17 +3,17 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val composeVersion = "1.4.0-alpha05"
-val composeCompilerVersion = "1.4.0"
+val composeVersion = "1.6.0-alpha02"
+val composeCompilerVersion = "1.5.1"
 
 android {
     namespace = "com.aliernfrog.pftool"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.aliernfrog.pftool"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 140
         versionName = "1.4.0"
         vectorDrawables { useSupportLibrary = true }
@@ -45,7 +45,7 @@ android {
         kotlinCompilerExtensionVersion = composeCompilerVersion
     }
 
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -53,16 +53,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha05")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("com.google.accompanist:accompanist-navigation-animation:0.29.0-alpha")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
-    implementation("com.github.aliernfrog:top-toast-compose:1.1.1")
-    implementation("com.lazygeniouz:dfc:0.91")
-    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha04")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    // TODO use androidx.navigation.compose
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.31.6-rc")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.31.6-rc")
+    implementation("com.github.aliernfrog:top-toast-compose:1.3.1")
+    implementation("com.lazygeniouz:dfc:1.0.7")
+    implementation("io.coil-kt:coil-compose:2.4.0")
 }
