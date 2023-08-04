ab18c36b7f282e25e95987408d4cb9a3747fd122
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-08-04 21:52:22 +0300

    fix(component/map-button): remember mapDetails

--- app/src/main/java/com/aliernfrog/pftool/ui/component/MapButton.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/component/MapButton.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -44,7 +45,7 @@ fun MapButton(
     onClick: () -> Unit
 ) {
     val context = LocalContext.current
-    val mapDetails = map.getDetails(context)
+    val mapDetails = remember { map.getDetails(context) }
     Box(Modifier.fillMaxWidth().height(IntrinsicSize.Max).padding(8.dp).clip(AppComponentShape).background(containerColor).clickableWithColor(contentColor) {
         onClick()
     }) {
