fb7ebad4055a602283d2b733041cad62cc0d322e
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-08-27 14:05:41 +0300

    refactor: remove unnecessary row

--- app/src/main/java/com/aliernfrog/pftool/ui/component/BaseScaffold.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/component/BaseScaffold.kt
@@ -9,13 +9,10 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Badge
@@ -38,7 +35,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.onSizeChanged
@@ -210,26 +206,19 @@ private fun NavigationItemIcon(
 @Composable
 private fun AppIcon() {
     val context = LocalContext.current
-    val appName = stringResource(R.string.app_name)
     val appIcon = remember {
         context.packageManager.getApplicationIcon(context.packageName)
             .toBitmap().asImageBitmap()
     }
-    Row(
+    Image(
+        bitmap = appIcon,
+        contentDescription = stringResource(R.string.app_name),
         modifier = Modifier
             .padding(bottom = 12.dp)
             .height(64.dp)
             .padding(
                 horizontal = 16.dp,
                 vertical = 8.dp
-            ),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            bitmap = appIcon,
-            contentDescription = appName,
-            modifier = Modifier.fillMaxHeight()
-        )
-    }
+            )
+    )
 }
