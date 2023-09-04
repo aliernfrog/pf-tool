9427429f58ab7dd076b9635176a0942f9166ede4
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-09-04 10:52:22 +0300

    refactor: remove unused import

--- app/src/main/java/com/aliernfrog/pftool/ui/viewmodel/MapsListViewModel.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/viewmodel/MapsListViewModel.kt
@@ -1,6 +1,5 @@
 package com.aliernfrog.pftool.ui.viewmodel
 
-import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
