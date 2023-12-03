8118593044819fb3dbd6efb54dad5f03803fc35e
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-12-03 22:01:58 +0300

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val composeVersion = "1.6.0-alpha05"
-val material3Version = "1.2.0-alpha07"
-val composeCompilerVersion = "1.5.3"
+val composeVersion = "1.6.0-alpha07"
+val material3Version = "1.2.0-alpha09"
+val composeCompilerVersion = "1.5.5"
 
 android {
     namespace = "com.aliernfrog.pftool"
@@ -62,11 +62,11 @@ dependencies {
     implementation("androidx.compose.material3:material3:$material3Version")
     implementation("androidx.compose.material3:material3-window-size-class:$material3Version")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.navigation:navigation-compose:2.7.2")
-    implementation("io.insert-koin:koin-androidx-compose:3.4.6")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("io.insert-koin:koin-androidx-compose:3.5.0")
     implementation("com.github.aliernfrog:top-toast-compose:1.3.4")
-    implementation("com.lazygeniouz:dfc:1.0.7")
-    implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation("com.github.jeziellago:compose-markdown:0.3.6")
+    implementation("com.lazygeniouz:dfc:1.0.8")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("com.github.jeziellago:compose-markdown:0.3.7")
 }
