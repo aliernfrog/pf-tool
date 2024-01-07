60f62d33c57ae7d4c5fac9c6f6bc5d59ae0d0e10
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2024-01-07 11:47:01 +0300

    fix: clear map selection on back button click

--- app/src/main/java/com/aliernfrog/pftool/ui/screen/maps/MapsScreen.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/screen/maps/MapsScreen.kt
@@ -1,5 +1,6 @@
 package com.aliernfrog.pftool.ui.screen.maps
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -47,6 +48,10 @@ fun MapsScreen(
 
     LaunchedEffect(mapsViewModel.chosenMap) {
         if (mapsViewModel.chosenMap == null) mapsViewModel.mapListShown = true
+    }
+
+    BackHandler(mapsViewModel.chosenMap != null) {
+        mapsViewModel.chooseMap(null)
     }
 
     AppScaffold(
