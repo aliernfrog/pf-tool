d01f63cee52addf3db0991909b43e7311ba696af
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2024-01-21 23:10:43 +0300

    chore: update app version to v1.6.0 (16000) & update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 val composeVersion = "1.6.0-alpha07"
 val material3Version = "1.2.0-alpha09"
-val composeCompilerVersion = "1.5.7"
+val composeCompilerVersion = "1.5.8"
 
 android {
     namespace = "com.aliernfrog.pftool"
@@ -15,8 +15,8 @@ android {
         applicationId = "com.aliernfrog.pftool"
         minSdk = 23
         targetSdk = 34
-        versionCode = 15100
-        versionName = "1.5.1"
+        versionCode = 16000
+        versionName = "1.6.0"
         vectorDrawables { useSupportLibrary = true }
     }
 
@@ -83,12 +83,12 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.compose.material3:material3:$material3Version")
     implementation("androidx.compose.material3:material3-window-size-class:$material3Version")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("io.insert-koin:koin-androidx-compose:3.5.3")
     implementation("com.github.aliernfrog:top-toast-compose:1.3.4")
     implementation("com.lazygeniouz:dfc:1.0.8")
     implementation("io.coil-kt:coil-compose:2.5.0")
-    implementation("com.github.jeziellago:compose-markdown:0.4.0")
+    implementation("com.github.jeziellago:compose-markdown:0.4.1")
 }
