b1868166e5eca14a24fcc06a6543d1c3b0d0d6ca
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-09-14 20:04:11 +0300

    fix(settings/storage): folder picker on raw path inputs

--- app/src/main/java/com/aliernfrog/pftool/ui/screen/settings/StoragePage.kt
+++ app/src/main/java/com/aliernfrog/pftool/ui/screen/settings/StoragePage.kt
@@ -129,11 +129,13 @@ private fun FolderConfiguration(
 ) {
     val context = LocalContext.current
     var activePref: PrefEditItem<String>? = remember { null }
-    val openFolderLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.OpenDocumentTree(), onResult = {
-        if (it == null) return@rememberLauncherForActivityResult
+    val openFolderLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.OpenDocumentTree(), onResult = { uri ->
+        if (uri == null) return@rememberLauncherForActivityResult
         val pref = activePref ?: return@rememberLauncherForActivityResult
-        it.takePersistablePermissions(context)
-        pref.preference(prefs).value = it.toString()
+        if (!useRawPathInputs) uri.takePersistablePermissions(context)
+        pref.preference(prefs).value = uri.toString().let {
+            if (useRawPathInputs) FileUtil.getFilePath(it) else it
+        }
     })
 
     AnimatedContent(useRawPathInputs) { rawPathInput ->
@@ -145,6 +147,7 @@ private fun FolderConfiguration(
                     label = label,
                     pref = pref,
                     onPickFolderRequest = {
+                        activePref = prefEditItem
                         openFolderLauncher.launch(null)
                     }
                 )
