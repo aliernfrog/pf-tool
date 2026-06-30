3a050689f992c9c2cc0b2cc5601c0595d13b5ae6
alieRN <45766489+aliernfrog@users.noreply.github.com>
2026-06-30 22:51:51 +0300

    chore: Update credits

--- shared/src/main/java/io/github/aliernfrog/shared/impl/CreditData.kt
+++ shared/src/main/java/io/github/aliernfrog/shared/impl/CreditData.kt
@@ -17,15 +17,16 @@ class CreditData(
     val fetchFromGithub: Boolean,
     val link: String? = if (fetchFromGithub) "https://github.com/$userName" else null
 ) {
-    private var fetchedOnce = false
+    var doneFetching = false
+        private set
 
     var avatarURL by mutableStateOf<String?>(null)
     var displayName by mutableStateOf(displayNameOverride ?: userName)
     var fetching by mutableStateOf(false)
 
     suspend fun fetchDetails() {
-        if (!fetchFromGithub || fetchedOnce) return
-        fetchedOnce = true
+        if (!fetchFromGithub || doneFetching) return
+        doneFetching = true
 
         fetching = true
         withContext(Dispatchers.IO) {
