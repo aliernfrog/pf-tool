f03f561d1a32d9301a4d7814960c21feb40289fe
alieRN <45766489+aliernfrog@users.noreply.github.com>
2026-07-12 14:02:13 +0300

    refactor: Update file sharing for upcoming Android changes https://developer.android.com/about/versions/17/behavior-changes-all#restrict-implicit-uri-grants

--- app/src/main/java/com/aliernfrog/pftool/util/staticutil/FileUtil.kt
+++ app/src/main/java/com/aliernfrog/pftool/util/staticutil/FileUtil.kt
@@ -1,5 +1,6 @@
 package com.aliernfrog.pftool.util.staticutil
 
+import android.content.ClipData
 import android.content.Context
 import android.content.Intent
 import androidx.core.content.FileProvider
@@ -23,18 +24,35 @@ class FileUtil {
         }
 
         fun shareFiles(vararg files: FileWrapper, context: Context, title: String = context.getString(R.string.action_share)) {
+            if (files.isEmpty()) return
+
             val isSingle = files.size <= 1
             val sharedFileUris = files.map {
                 FileProvider.getUriForFile(context, "${context.packageName}.provider", moveToSharedCache(it, context))
             }
             val firstUri = sharedFileUris.first()
+            val mimeType = context.contentResolver.getType(firstUri)
+
             val intent = Intent(
                 if (sharedFileUris.size > 1) Intent.ACTION_SEND_MULTIPLE else Intent.ACTION_SEND
-            )
-                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                .setDataAndType(firstUri, context.contentResolver.getType(firstUri))
-            if (isSingle) intent.putExtra(Intent.EXTRA_STREAM, firstUri)
-            else intent.putExtra(Intent.EXTRA_STREAM, ArrayList(sharedFileUris))
+            ).apply {
+                type = mimeType
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+                val clip = ClipData.newRawUri(null, firstUri)
+                if (!isSingle) for (i in 1 until sharedFileUris.size) {
+                    clip.addItem(ClipData.Item(sharedFileUris[i]))
+                }
+                clipData = clip
+
+                if (isSingle) {
+                    data = firstUri
+                    putExtra(Intent.EXTRA_STREAM, firstUri)
+                } else {
+                    putParcelableArrayListExtra(Intent.EXTRA_STREAM, ArrayList(sharedFileUris))
+                }
+            }
+
             context.startActivity(Intent.createChooser(intent, title))
         }
 
